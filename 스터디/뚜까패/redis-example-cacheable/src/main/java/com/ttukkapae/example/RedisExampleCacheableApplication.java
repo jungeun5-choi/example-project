869ebb89b089e501ed247ac518479cfb51cdd352
jungeun-choi <jungeun5.choi@gmail.com>
2023-07-30 02:45:42 +0900

    [스터디][뚜까패] Initial commit

---
+++ 스터디/뚜까패/redis-example-cacheable/src/main/java/com/ttukkapae/example/RedisExampleCacheableApplication.java
@@ -0,0 +1,13 @@
+package com.ttukkapae.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExampleCacheableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleCacheableApplication.class, args);
+    }
+
+}
---
+++ 스터디/뚜까패/redis-example-cacheable/src/test/java/com/ttukkapae/example/RedisExampleCacheableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttukkapae.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExampleCacheableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
